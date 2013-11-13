1cca16b972aa8d22b13880573695b2869a0f4e4b
Parranoia <alecx315@aim.com>
2013-11-12 23:36:35 -0500

    Updated the users table to have a unique username and email address
    Added .DS_STORE to .gitignore

--- sql/create.sql
+++ sql/create.sql
@@ -1,10 +1,35 @@
-CREATE DATABASE RCD;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.11 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.0.0.4396
+-- --------------------------------------------------------
 
-CREATE TABLE users(
-id INT NOT NULL AUTO_INCREMENT ,
-username VARCHAR( 16 ) NOT NULL ,
-email VARCHAR( 255 ) NOT NULL ,
-PASSWORD VARCHAR( 255 ) NOT NULL ,
-salt VARCHAR( 255 ) NOT NULL ,
-PRIMARY KEY ( id )
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for rcd
+DROP DATABASE IF EXISTS `rcd`;
+CREATE DATABASE IF NOT EXISTS `rcd` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `rcd`;
+
+
+-- Dumping structure for table rcd.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
