e0aa21710e28155402669b2f6149092f8870ba7a
Parranoia <alecx315@aim.com>
2013-11-28 21:07:42 -0500

    Finished changing passwords. Works on correct information. Needs to be tested for bad input.

--- pages/recover_password.php
+++ pages/recover_password.php
@@ -5,6 +5,8 @@ if (!isset($_GET['email']) || !isset($_GET['key']))
     die();
 }
 
+$error = "";
+
 $email = $_GET['email'];
 $key = $_GET['key'];
 
@@ -15,17 +17,101 @@ $query_params = array(':email' => $email,
  try
  {
      $stmt = $db->prepare($query);
-     $result = $db->execute($query_params);
+     $result = $stmt->execute($query_params);
  }
  catch(PDOException $e)
  {
      die();
  }
  
- // If a row was returned
+ // This is a valid request
  if ($stmt->fetch())
  {
-     
+     // Check if this is a post request or not
+    if (!empty($_POST))
+    {
+        $pass = $_POST['pass'];
+        $pass_confirm = $_POST['pass_confirm'];
+         
+        if (!preg_match('/^[a-zA-Z0-9.-_!]{8,18}$/', $pass))
+        {
+            if (strlen($pass) < 8 || strlen($pass) > 18)
+                $error = "Password must be between 8 and 18 characters long";
+            else
+            {
+                // Find out what characters failed the check
+                $invalid_chars = preg_replace('/([a-zA-Z0-9.\-_!]*)/', '', $pass);
+                $char_arr = str_split($invalid_chars);
+                
+                // Remove duplicates
+                $char_arr = array_unique($char_arr);
+                
+                $msg = "Password cannot contain the following: ";
+                foreach ($char_arr as $char)
+                    $msg .= $char . ' ';
+                $msg = $substr($msg, 0, strlen($msg) - 2);
+                $error = $msg;
+            }
+        }
+        else if ($pass !== $pass_confirm)
+            $error = "Passwords do not match";
+        
+        // If there was no error, proceed with processing
+        if (!$error)
+        {
+            // Generate a new salt and encrypt a new password
+            $salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
+            $password = hash('sha256', $pass . $salt);
+            
+            $query = "UPDATE users SET password = :password, salt = :salt WHERE email = :email";
+            $query_params = array(':password' => $password,
+                                  ':salt' => $salt,
+                                  ':email' => $email);
+                                  
+            try
+            {
+                $stmt = $db->prepare($query);
+                $result = $stmt->execute($query_params);
+            }
+            catch (PDOException $e)
+            {
+                die();
+            }
+            
+            // Delete the row from forgotten password
+            $query = "DELETE FROM forgot_password WHERE user = (SELECT id FROM users WHERE email = :email)";
+            $query_params = array(':email' => $email);
+            
+            try
+            {
+                $stmt = $db->prepare($query);
+                $result = $stmt->execute($query_params);
+            }
+            catch (PDOException $e)
+            {
+                die();
+            }
+            
+            print("<div class=\"postinfo\">Your password has been updated!<br>You will be redirected in 5 seconds</div>");
+            
+            header("refresh:5;url=http://" . $_SERVER['SERVER_NAME'] . "/login");
+        }
+    }
  }
+else // Redirect them since this is not a valid key and email combination
+{
+    header("Location: http://" . $_SERVER['SERVER_NAME']);
+    die();
+}
 
 ?>
+                    <div class="forminfo">Please enter your new password</div>
+                    <form class="centerform" method="POST" action="/account/recover_password?email=<?php echo $email?>&key=<?php echo $key?>">
+<?php
+    if ($error)
+        print("\t\t\t<div class=\"error\">" . $error . "</div>\n");
+?>
+                        <input type="password" name="pass" placeholder="New Password" />
+                        <input type="password" name="pass_confirm" placeholder="Confirm Password" />
+                        <input type="submit" value="Submit" />
+                    </form>
