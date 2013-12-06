bcff21828dc54d3c698dc8121a573a29283c3e0a
Parranoia <alecx315@aim.com>
2013-12-05 23:24:55 -0500

    Improved upon the dynamic forms. A few more fields still need to be added, but the core work is completed

--- pages/interested.php
+++ pages/interested.php
@@ -9,19 +9,33 @@
 			</div>
 			<script>
 				$(document).ready(function() {
-					$('#num_children').change(function() {
-						$('#interested').append('<fieldset><legend>Temp</legend></fieldset>');
-					});
+				    $('#num_children').change(function() {
+				        var temp = $('#childForm_1').wrap('<fieldset>').parent();  
+				        $('#parentForm').nextAll().remove();
+				        var num = $('#num_children').val();
+				        for (var i = 1; i <= num; i++) {
+				        	temp.children().attr('id', 'childForm_' + i);
+				            temp.children().children('legend:first').text('Child #' + i);
+				            temp.children().children(':nth-child(2)').attr('name', 'child_name' + i);
+				            temp.children().children(':nth-child(3)').attr('name', 'child_age' + i);
+				            $('#interested').append(temp.html());
+				        }
+				    });
 				});
 			</script>
 			<form id="interested" class="centerform" method="POST" action="/interested">
-				<fieldset>
+				<fieldset id="parentForm">
 					<legend>Parent Information</legend>
 					<input type="text" name="parent_name" placeholder="Parent's full name">
 					<input type="text" name="email" placeholder="Email Address">
 					<span class="small">How many children are you interested in enrolling?</span>
 					<select id="num_children" required>
-<?php for ($i = 1; $i < 11; $i++) print("\t\t\t\t\t\t<option>" . $i . "</option>\n"); ?>
+<?php for ($i = 1; $i < 11; $i++) print("\t\t\t\t\t\t<option value=\"$i\">$i</option>\n"); ?>
 					</select>
+				</fieldset>
+				<fieldset id="childForm_1">
+					<legend>Child # 1</legend>
+					<input type="text" name="child_name1" placeholder="Child's full name">
+					<input type="text" name="child_age1" placeholder="Child's age">
 				</fieldset>
 			</form>
