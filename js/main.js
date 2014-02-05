a19a51375fb82a5d749c6ac91313024d7d625fda
Parranoia <alecx315@aim.com>
2014-02-04 21:23:42 -0500

    Fixed an issue with datepicker on the interested page and began validation for the form

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
  */
 $(document).ready(function() {
 	$('#num_children').change(function() {
-	    $('.dob').datepicker("destroy");
+	    $('.dob').datepicker('destroy');
 	    var temp = $('#childForm_1').wrap('<fieldset>').parent();  
 	    $('#parentForm').nextAll().remove();
 	    var num = $('#num_children').val();
@@ -37,19 +37,24 @@ $(document).ready(function() {
 	        temp.children().attr('id', 'childForm_' + i);
 	        temp.children().children('legend:first').text('Child #' + i);
 	        temp.children().children(':nth-child(2)').attr('name', 'child_name_' + i);
-	        temp.children().children(':nth-child(3)').attr('name', 'dob_' + i);
+	        temp.children().children(':nth-child(3)').attr('name', 'dob_' + i).attr('id', 'dob_' + i);
 	        temp.children().children(':nth-child(4)').attr('name', 'gender_' + i);
 	        $('#interested').append(temp.html());
+	        
+	        $('#dob_' + i).datepicker({
+		        yearRange: '-20:+0',
+		        maxDate: 0,
+		        changeMonth: true,
+		        changeYear: true,
+		        showAnim: 'clip'
+	    	});
 	    }
-	    $('.dob').datepicker({
-	        yearRange: '-20:+0',
-	        maxDate: 0,
-	        changeMonth: true,
-	        changeYear: true,
-	        showAnim: 'clip'
-	    });
+	    
+	    var submitButton = $(document.createElement('input')).attr('type', 'submit').attr('value', 'Submit');
+	    $('#interested').append(submitButton);
+	    
 	});
-	$('.dob').datepicker({
+	$('#dob_1').datepicker({
 	    yearRange: '-20:+0',
 	    maxDate: 0,
 	    changeMonth: true,
--- pages/interested.php
+++ pages/interested.php
@@ -1,5 +1,31 @@
 <?php
 
+$errors = array();
+
+if (!empty($_POST))
+{
+	if (empty($_POST['parent_name']))
+		$errors['parent_name'] = 'Please enter your full name';
+	else
+		if (!preg_match('/^[a-zA-Z \']+$/', $_POST['parent_name']))
+			$errors['parent_name'] = 'The name you entered is invalid';
+	
+	if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+        $errors["email"] = "Invalid email";
+	
+	if (!preg_match('/^[a-zA-Z0-9 \'&\-]*$/', $_POST['employer']))
+		$errors['employer'] = 'Invalid employer. You entered a character that is not accepted';
+	
+	$num_children = $_POST['num_children'];
+	
+	for ($i = 1; $i <= $num_children; $i++)
+	{
+		$name = $_POST['child_name_' . $i];
+		$dob = $_POST['dob_' . $i];
+		$gender = $_POST['gender_' . $i];
+	}
+}
+
 ?>
 			<h1>Interested in Radford Child Development?</h1>
 			<div class="center">
@@ -10,9 +36,9 @@
 			<form id="interested" class="centerform" method="POST" action="/interested">
 				<fieldset id="parentForm">
 					<legend>Parent Information</legend>
-					<input type="text" name="parent_name" placeholder="Parent's full name" required>
-					<input type="text" name="email" placeholder="Email Address" required>
-					<input type="text" name="employer" placeholder="Current Employer">
+					<input type="text" name="parent_name" placeholder="Parent's full name" maxlength="50" required>
+					<input type="text" name="email" placeholder="Email Address" maxlenth="50" required>
+					<input type="text" name="employer" placeholder="Current Employer" maxlength="100">
 					<span class="small">How many children are you interested in enrolling?<br></span>
 					<select id="num_children" name="num_children" required>
 						<option value="1">1</option>
@@ -24,12 +50,13 @@
 					</select>
 				</fieldset>
 				<fieldset id="childForm_1">
-					<legend>Child # 1</legend>
-					<input type="text" name="child_name_1" placeholder="Child's full name">
-					<input type="text" class="dob" name="dob_1" placeholder="Date of Birth">
-					<select name="gender_1">
+					<legend>Child #1</legend>
+					<input type="text" name="child_name_1" placeholder="Child's full name" maxlength="50" required>
+					<input id="dob_1" type="text" class="dob" name="dob_1" placeholder="Date of Birth" maxlength="12" required>
+					<select name="gender_1" required>
 						<option value="Male">Male</option>
 						<option value="Female">Female</option>
 					</select>
 				</fieldset>
+				<input type="submit" value="Submit" />
 			</form>
