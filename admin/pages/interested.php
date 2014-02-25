3af9624d0c5b28511669923caeda3170be525ffd
Parranoia <alecx315@aim.com>
2014-02-25 15:21:00 -0500

    Added a short jquery script to make collapsible divs

--- admin/pages/interested.php
+++ admin/pages/interested.php
@@ -2,4 +2,14 @@
 
 ?>
                 <h1>Interested Page...</h1>
+                <div class="toggle">Parent 1</div>
+                <div>This text is hidden</div>
+                <div class="toggle">Parent #2</div>
+                <div>This is the second parent's kid</div>
                 
+                <script>
+                $('.toggle').next().hide();
+                $('.toggle').click(function() {
+                    $(this).next().slideToggle("slow"); 
+                });
+                </script>
