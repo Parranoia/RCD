6532ef0501f675658fdfae18784f00c2c6e273d9
Parranoia <alecx315@aim.com>
2014-02-26 13:15:01 -0500

    Interested page should pull data from the database now

--- admin/pages/interested.php
+++ admin/pages/interested.php
@@ -1,46 +1,84 @@
 <?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/include/Child.class.php');
+
+/*
+ * Parent has a full name, employer, email, and number of children
+ * The child has a full name, dob, and a gender
+ */
+$query = 'SELECT id, name, email, employer, num_children FROM interested_parents ORDER BY id ASC';
+try
+{
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+}
+catch (PDOException $e)
+{
+    die();
+}
+$parents = $stmt->fetchAll();
+
+$query = 'SELECT parent, name, dob, gender FROM interested_children ORDER BY parent ASC';
+try
+{
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+}
+catch (PDOException $e)
+{
+    die();
+}
+
+$rows = $stmt->fetchAll();
+$children = array(array());
+
+foreach ($rows as $row)
+{
+    $name = $row['name'];
+    $dob = $row['dob'];
+    $gender = $row['gender'];
+    $parent = $row['parent'];
+    $children[$parent][] = new Child($name, $dob, $gender);
+}
+
+function print_interested_list($parents, $children)
+{
+    foreach ($parents as $parent)
+    {
+        echo '<div class="parent_info">';
+        echo '<p>' . $parent['name'] . '</p>';
+        echo '<p>' . $parent['email'] . '</p>';
+        if ($parent['employer']) echo '<p>' . $parent['employer'] . '</p>';
+        echo '<p>' . $parent['num_children'] . ' Children</p>';
+        echo '<i class="fa fa-chevron-right"></i>';
+        echo '</div>';
+        
+        echo '<div class="child_info">';
+        echo '<ul>';
+        
+        $temp = $children[$parent['id']];        
+        foreach ($temp as $child)
+        {
+            echo '<li>';
+            echo $child->getName();
+            echo '<ul>';
+            echo '<li>' . $child->getDob() . '</li>';
+            echo '<li>' . $child->getGender() . '</li>';
+            echo '</ul>';
+            echo '<li>';            
+        }
+        echo '</ul>';
+        echo '</div>';
+    }
+}
 
 ?>
                 <h1>Interested Page...</h1>
-                <div class="parent_info">
-                    <p>Parent 1</p>
-                    <p>Radford University</p>
-                    <p>4 Children</p>
-                    <i class="fa fa-chevron-right"></i>
-                </div>
-                <div class="child_info">
-                    <ul>
-                        <li>
-                            Child 1
-                            <ul>
-                                <li>DOB: 1/18/13</li>
-                                <li>Gender: Male</li>
-                            </ul>    
-                        </li>
-                        <li>Child 2</li>
-                        <li>Child 3</li>
-                        <li>Child 4</li>
-                    </ul>
-                </div>
-                <div class="parent_info">
-                    <p>Parent 2</p>
-                    <p>Costco</p>
-                    <p>2 Children</p>
-                    <i class="fa fa-chevron-right"></i>
-                </div>
-                <div class="child_info">
-                    <ul>
-                        <li>Child 1</li>
-                        <li>Child 2</li>
-                    </ul>
-                </div>
+                <?php print_interested_list($parents, $children); ?>
                 
                 <script>
-                
                 $('.parent_info').click(function() {
-                    $(this).next().slideToggle('slow');
-                    //$(this).next().width($(this).width()); 
+                    $(this).next().slideToggle('slow'); 
                     $(this).toggleClass('toggled');
-                    $(this).children(':nth-child(4)').toggleClass('fa-chevron-right fa-chevron-down');
+                    $(this).children('.fa').toggleClass('fa-chevron-right fa-chevron-down');
                 });
                 </script>
