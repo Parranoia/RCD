4b6b0350ba91628d1da8e2375c56515b1f87d4b7
Parranoia <alecx315@aim.com>
2014-02-27 13:46:40 -0500

    Applied changes to donate and about pages

--- pages/about.php
+++ pages/about.php
@@ -3,7 +3,36 @@
 					
                     <h3>Who are we?</h3>
                     <p>
-                        Radford Child Development, Inc. is a non-profit organization determined to develop a quality, certified child care facility in Radford. Based on our needs assessment, we concluded that many members of the Radford community are struggling to find a decent, nationally certified child care for their children. 
+                        Radford Child Development, Inc. (RCD) is a grassroots, community-led, non-profit organization established in 2012 to develop a quality, certified child care facility in Radford. 
+                        It is run by a diverse board of directors who devote their time, expertise, and personal funds to support an organizational mission that is critical to the Radford community. 
+                        Based on an extensive needs assessment conducted last year, at least 9 out of 10 children in the City of Radford do not have access to state-licensed child care. 
+                        Working parents are struggling to find child care that even meets minimum state licensing standards.  
+                        There is no option for quality certified child care in the community.  
+                        Lack of access to child development facilities has a long-lasting negative impact on our community. 
+                        According to research, providing quality early childhood education is an effective strategy for moving people out of poverty. 
+                        Bringing a quality, education-based facility to Radford is a long term investment that will:
+                        <ol>
+                            <li>Enable Radford families to have access to the essential educational foundation of quality childcare</li>
+                            <li>Provide necessary community facilities and infrastructure for working parents in order to improve their own lives and make contributions to our community</li>
+                            <li>Bring fundamental and lasting change to the entire Radford community and our low-wealth community members. It also strengthens the community by supporting economic growth and making our area more attractive for new businesses and housing</li>
+                        </ol>
+                    </p>
+                    <p>
+                        Our Mission is to develop and/or support excellent, nationally accredited child development program(s) in Radford that embodies the best practices of early childhood care and education. 
+                        One of our fundamental goals is to make this accessible to all income levels in our community. 
+                        We believe that providing low-wealth families with access to a high level educational environment for their children will not only improve kid’s chances to succeed academically and move out of poverty, but will also have an immediate impact on the family finances by providing parents with time to work, knowing that children are in a safe and stimulating environment.
+                    </p>
+                    <h3>Major Strategies</h3>
+                    <p>
+                        RCD is a non-profit organization that was established by members of the Radford community to address an immediate and essential need for all Radford community members. We understand that all working parents need to have access to a quality certified educational child development facility. Our major strategies include:
+                        <ol>
+                            <li>Attracting a quality nationally certified child care provider to Radford, VA</li>
+                            <li>Providing external funds for pre-opening expenses to make operating a center financially feasible</li>
+                            <li>Providing tuition discounts to low-wealth families</li>
+                            <li>Creating awareness, disseminating information, and involving all community members</li>
+                            <li>Recruiting and retaining highly qualified teachers</li>
+                            <li>Working with United States and Commonwealth representatives to change local/state/federal policies related to child development facilities</li>
+                        </ol>    
                     </p>
                     <h3>Benefits of a Radford Child Development Center</h3>
                     <ul>
@@ -22,6 +51,19 @@
                     <h3>Why does this matter to you?</h3>
                     <p>
                         Once 1023 approved, it is inclusive of all donations taken in from the incorporation date to the date of 501c3 approval. Basically, this means that your donations are tax-deductible. 
+                    </p>
+                    <h3>Strategic Partners</h3>
+                    <p>
+                        <ol>
+                            <li>Community Foundation of New River Valley</li>
+                            <li>Radford University Foundation</li>
+                            <li>Price Williams Realtors</li>
+                            <li>Radford City</li>
+                            <li>Radford Public Schools</li>
+                            <li>DIVAS</li>
+                            <li>Rainbow Riders</li>
+                            <li>Smart Beginnings</li>
+                        </ol>
                     </p>
                     <h3>Contact Us</h3>
                     <p>
