e4e360586feef2457b8a75e221796c91b0d551d1
Parranoia <alecx315@aim.com>
2014-03-25 15:01:32 -0400

    Refactored some of the javascript

---
+++ admin/js/manage_pages.js
@@ -0,0 +1,100 @@
+function managePageAjax(data) {
+    return $.ajax({ 
+        url: '/admin/pages/manage_pages.php',
+        type: 'POST',
+        data: data 
+    });
+}
+
+
+var submitting = false;
+
+$('.page_info p:first-child').on('click', function() {
+    if (submitting)
+        return;
+        
+    var page = $(this);
+    var editor = $('.page_content');
+    if (page.hasClass('active'))
+    {
+        page.toggleClass('active');
+        page.parent().toggleClass('active');
+        // Get rid of textarea and submit data
+        var data = $.param({ page: page.html().toLowerCase(), content: tinyMCE.activeEditor.getContent() });
+        submitting = true;
+        managePageAjax(data).done(function() { 
+            submitting = false;
+            editor.animate({ opacity: 0 }, 600); 
+            });
+    }
+    else
+    {
+        // Save any other editor that might have been open
+        var active = $('.active p');
+        if (active.length)
+        {
+            var data = $.param({ page: active.html().toLowerCase(), content: tinyMCE.activeEditor.getContent() });
+            submitting = true;
+            managePageAjax(data).done(function() { 
+                submitting = false;
+             });
+        }
+        
+        $('.active').removeClass('active');
+        var data = $.param({ request_page: page.html().toLowerCase() });
+        submitting = true;
+        
+        managePageAjax(data).done(function(response) { 
+            submitting = false;
+            if (response.length) {
+                tinyMCE.activeEditor.setContent(response);
+                editor.animate({ opacity: 1 }, 600);    
+                page.toggleClass('active');
+                page.parent().toggleClass('active');
+            } else {
+                editor.animate({ opacity: 0 }, 600);
+                alert('This is a custom page and cannot be edited');
+            }
+        });
+    }
+});
+
+var animating = false;
+                    
+$('.page_info i').on('click', function() {
+    if (animating)
+        return;
+        
+    var container = $(this).parents('.page_info');
+    var distance = container.outerHeight();
+    var thisPage = $(this).parent().prev().html().toLowerCase();
+    // Check if this div needs to move up or down
+    var up = $(this).hasClass('fa-arrow-up');
+
+    var other = up ? $(container).prev() : $(container).next();
+    if (other.length == 0)
+        return;
+        
+    var otherPage = $($(other).children()[0]).html().toLowerCase();
+    
+    animating = true;
+    $.when(container.animate({
+        top: up ? -distance - 5 : distance + 5
+    }, 600),
+    other.animate({
+        top: up ? distance + 5 : -distance - 5
+    }, 600)).done(function() {
+        other.css('top', '0px');
+        container.css('top', '0px');
+        up ? container.insertBefore(other) : container.insertAfter(other);
+        animating = false;
+    });
+    
+    var data;
+    if (up)
+        data = $.param({ this_page: thisPage, prev_page: otherPage });
+    else
+        data = $.param({ this_page: thisPage, next_page: otherPage });
+    
+    managePageAjax(data);    
+});
