c334ad530fc5c0974061b795897f4cefd075ea0d
Parranoia <alecx315@aim.com>
2014-03-31 17:53:33 -0400

    Removed all external css and js

--- include/header.php
+++ include/header.php
@@ -2,14 +2,13 @@
 <html>
     <head>
         <title>Radford Child Development</title>
-        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="/css/smoothness/jquery-ui.min.css">
         <link rel="stylesheet" type="text/css" href="/css/default.css">
         <script type="text/javascript" src="/js/jquery-1.11.0.min.js"></script>
-        <script type="text/javascript" src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="/js/main.js"></script>
         <meta charset="utf-8">
         <meta name="keywords" content="radford, radford childcare, childcare, child care, daycare, radford daycare, radford child development, daycare">
-        
-        <script type="text/javascript" src="/js/main.js"></script>
     </head>
     <body>
         <?php 
